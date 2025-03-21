6d7e688d5b2653541b9fa911138ff525c29ced9e
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-03-21 15:05:35 +0530

    Backgroung change app functionality

--- App.tsx
+++ App.tsx
@@ -5,13 +5,14 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
@@ -24,108 +25,52 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [BGcolor,setBGcolor] = useState('#000000')
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const generateBG =()=>{
+    const HEXrange = "0123456789ABCDEF";
+    let color = "#";
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
-
+    for (let i = 0; i < 6; i++) {
+      color += HEXrange[Math.floor(Math.random() * 16)]
+    }
+    setBGcolor(color)
+  }
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </View>
+    <>
+      <StatusBar backgroundColor={BGcolor}/>
+        <View style={[styles.Container,{backgroundColor:BGcolor}]}> 
+          <TouchableOpacity onPress={generateBG }>
+            <View style={styles.ActionBtn}>
+              <Text style={[styles.ActionBtnText]}>
+                Press me
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View> 
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  Container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  ActionBtn:{
+    borderRadius : 12, 
+    backgroundColor:'#6A1B4D',
+    paddingVertical:10,
+    paddingHorizontal:40,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  ActionBtnText:{
+    color:'#fff',
+    fontSize:20,
+    textTransform:'uppercase',
+  }
 });
 
 export default App;
