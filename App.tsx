141ede6c2816028cae943d705a628a73d3a30111
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-03-22 10:26:12 +0530

    [added Some Shapes that changes colour as well]

--- App.tsx
+++ App.tsx
@@ -6,42 +6,56 @@
  */
 
 import React, { useState } from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   TouchableOpacity,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-
 function App(): React.JSX.Element {
   const [BGcolor,setBGcolor] = useState('#000000')
+  const [BGcolor1,setBGcolor1] = useState('#ffffff')
+  const [BGcolor2,setBGcolor2] = useState('#ffffff')
+  const [BGcolor3,setBGcolor3] = useState('#ffffff')
+  const [BGcolor4,setBGcolor4] = useState('#ffffff')
 
   const generateBG =()=>{
     const HEXrange = "0123456789ABCDEF";
     let color = "#";
+    let color1 = "#";
+    let color2 = "#";
+    let color3 = "#";
+    let color4 = "#";
 
     for (let i = 0; i < 6; i++) {
       color += HEXrange[Math.floor(Math.random() * 16)]
     }
+    for (let i = 0; i < 6; i++) {
+      color1 += HEXrange[Math.floor(Math.random() * 16)]
+    }
+    for (let i = 0; i < 6; i++) {
+      color2 += HEXrange[Math.floor(Math.random() * 16)]
+    }
+    for (let i = 0; i < 6; i++) {
+      color3 += HEXrange[Math.floor(Math.random() * 16)]
+    }
+    for (let i = 0; i < 6; i++) {
+      color4 += HEXrange[Math.floor(Math.random() * 16)]
+    }
     setBGcolor(color)
+    setBGcolor1(color1)
+    setBGcolor2(color2)
+    setBGcolor3(color3)
+    setBGcolor4(color4)
   }
   return (
     <>
       <StatusBar backgroundColor={BGcolor}/>
         <View style={[styles.Container,{backgroundColor:BGcolor}]}> 
+        <View style={[styles.Triangle1,{borderBottomColor:BGcolor1}]}></View>
+        <View style={[styles.Square,,{backgroundColor:BGcolor2}]}></View>
           <TouchableOpacity onPress={generateBG }>
             <View style={styles.ActionBtn}>
               <Text style={[styles.ActionBtnText]}>
@@ -49,6 +63,8 @@ function App(): React.JSX.Element {
               </Text>
             </View>
           </TouchableOpacity>
+        <View style={[styles.Triangle2,{borderBottomColor:BGcolor3}]}></View>
+        <View style={[styles.Circle,{backgroundColor:BGcolor4}]}></View>
         </View> 
     </>
   );
@@ -70,6 +86,47 @@ const styles = StyleSheet.create({
     color:'#fff',
     fontSize:20,
     textTransform:'uppercase',
+  },
+  Triangle1:{
+    position:'absolute',
+    top:100,
+    left:75,
+    width: 0,
+	  height: 0,
+    borderLeftColor :'transparent',
+    borderLeftWidth:25,
+    borderRightColor :'transparent',
+    borderRightWidth:25,
+    // borderBottomColor : "#fff",
+    borderBottomWidth:50,
+  },
+  Triangle2:{
+    position:'absolute',
+    bottom:100,
+    right:75,
+    width: 0,
+	  height: 0,
+    borderLeftColor :'transparent',
+    borderLeftWidth:25,
+    borderRightColor :'transparent',
+    borderRightWidth:25,
+    // borderBottomColor : "#fff",
+    borderBottomWidth:50,
+  },
+  Square:{
+    position:'absolute',
+    top:200,
+    right:75,
+    height: 50,
+    width: 50,
+  },
+  Circle:{
+    position:'absolute',
+    bottom:250,
+    left:100,
+    height: 50,
+    width: 50,
+    borderRadius:'50%',
   }
 });
 
